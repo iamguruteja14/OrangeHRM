24e5d228c5509d67ccd6abd0c3704c258fcb142c
Iamguruteja <gurutejaguruteja14@gmail.com>
2025-12-15 13:17:17 +0530

    Updated test cases

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,8 +2,8 @@ import{test,expect} from '@playwright/test'
 test('verify the login functionalities',async({page})=>{
     test.setTimeout(120000)
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    await page.locator('//input[@name="username"]').fill('Admin')
-    await page.locator('//input[@type="password"]').fill('admin123')
+    await page.locator('//input[@name="username"]').fill(process.env.APP_USERNAME)
+    await page.locator('//input[@type="password"]').fill(process.env.APP_PASSWORD)
     await page.locator('//button[text()=" Login "]').click()
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
     await expect(page.getByAltText('client brand banner')).toBeVisible()
